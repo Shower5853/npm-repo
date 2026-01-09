f5a829175393a597af197fcda088eb8095bac12d
Shower5853 <kotrpa0192@gmail.com>
2026-01-09 22:36:08 +0400

    idk

--- wefetch/install.sh
+++ wefetch/install.sh
@@ -1,9 +1,33 @@
-wget -O /tmp/wefetch_package_npm https://raw.githubusercontent.com/Shower5853/npm-repo/refs/heads/main/wefetch/wefetch
+#!/bin/bash
+
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+
+wget -O /tmp/wefetch_package_npm https://raw.githubusercontent.com/Shower5853/npm-repo/refs/heads/main/wefetch/wefetch >/dev/null 2>&1
+echo "Binary file downloaded!"
+
 sleep 1
-mv /tmp/wefetch_package_npm /usr/bin/wefetch
-wget -O /tmp/npm-repo.zip https://github.com/Shower5853/npm-repo/archive/refs/heads/main.zip
-unzip /tmp/npm-repo
-mkdir "${XDG_CONFIG_HOME}/wefetch"
-mv /tmp/npm-repo-main/wefetch/logos "${XDG_CONFIG_HOME}/wefetch/"
-rm -rf /tmp/npm-repo.zip /tmp/npm-repo-main
+
+sudo mv /tmp/wefetch_package_npm /usr/bin/wefetch
+echo "Binary file moved to /usr/bin/wefetch"
+
+sudo chmod +x /usr/bin/wefetch
+echo "chmod +x"
+
+wget -O /tmp/wefetch_logos.zip https://raw.githubusercontent.com/Shower5853/npm-repo/refs/heads/main/wefetch/logos.zip >/dev/null 2>&1
+echo "Downloaded logos archive"
+
+mkdir -p "${XDG_CONFIG_HOME}/wefetch/"
+echo "Created ~/.config/wefetch/"
+
+mv /tmp/wefetch_logos.zip "${XDG_CONFIG_HOME}/wefetch/"
+echo "Logos archive moved to ~/.config/wefetch/"
+
+unzip "${XDG_CONFIG_HOME}/wefetch/logos.zip"
+echo "Unzipped logos"
+
+rm -rf "${XDG_CONFIG_HOME}/wefetch/logos.zip"
+echo "Deleted logos archive"
+
+echo
+
 echo "Wefetch has been succesfully installed!"
